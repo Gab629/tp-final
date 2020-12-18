892421f65b13dc4c24df4be117368b9b7a4fae5c
Gabriel <gab.gauthier2002@gmail.com>
2020-12-18 15:47:36 -0500

    html version 2 menu mobile termine

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,47 @@
+
 document.addEventListener("DOMContentLoaded", function(event) {
 
     //Un commentaire
     console.log("Ça fonctionne!!!");
 
+    var hamburger = document.querySelector(".hamburger-animated");
+    var menuMobile = document.querySelector(".nav-primary-mobile");
+
+    hamburger.addEventListener("click", openMenu);
+    console.log("Oy!!!");
+
+
+
+    function openMenu(evt) {
+        evt.preventDefault();
+
+        var cible = evt.currentTarget;
+
+
+        if (cible.classList.contains("open")) {
+            cible.classList.remove("open");
+            menuMobile.classList.remove("open");
+        } else {
+            cible.classList.add("open");
+            menuMobile.classList.add("open");
+        }
+    }
+
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        autoplay: {
+            delay: 5000,
+        },
+
+
+    });
+
 
 });
+
