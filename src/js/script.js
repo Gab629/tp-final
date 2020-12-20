5f9e8a05a6fb2d259ede2ad006781627c27cdf37
Gabriel <gab.gauthier2002@gmail.com>
2020-12-20 14:31:15 -0500

    Base de donnee fonctionnelle index.html

--- src/js/script.js
+++ src/js/script.js
@@ -48,6 +48,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     });
 
+    /* ------------------------------- Base de donee -------------------------------------*/
+
+    let connexion = new MovieDB();
+
+    if(document.location.pathname.search('fiche-film.html') > 0){
+        let params = new URL(document.location).searchParams;
+        connexion.requeteInfoFilms(params.get('id'));
+    }else{
+        connexion.requeteDernierFilms();
+        connexion.requeteDernierFilmsCarrousel();
+    }
+
 
 });
 
@@ -61,10 +73,155 @@ function retourHaut() {
 }
 
 
+/* ------------------------------- Base de donee -------------------------------------*/
+
+class MovieDB {
+
+    constructor() {
+        console.log('new MovieDB()');
+
+        this.apikey = "3c367a023e1abbda15e9e3c3b0db2d8b";
+        this.lang = "fr-CA";
+        this.baseUrl = "https://api.themoviedb.org/3/";
+        this.imgPath = "https://image.tmdb.org/t/p/";
+        this.totalFilm = 9;
+
+    }
+
+    requeteDernierFilms() {
+        let requette = new XMLHttpRequest();
+        requette.addEventListener('loadend', this.retourRequeteDernierFilm.bind(this));
+        requette.open('GET', this.baseUrl + 'movie/top_rated?api_key=' + this.apikey + '&language=' + this.lang + '&page=1');
+        requette.send();
+    };
+
+    retourRequeteDernierFilm(event) {
+        console.log('ca marche');
+        let target = event.currentTarget;
+        let data = JSON.parse(target.responseText).results;
+        //console.log(data);
+
+        this.afficheDernierFilm(data);
+
+    };
+
+    afficheDernierFilm(data) {
+        let section = document.querySelector('#liste-films');
+        console.log(section);
+
+        for (let i = 0; i < this.totalFilm; i++) {
+            //console.log(data[i].title);
+            // console.log(data[i].overview);
+            let article = document.querySelector('.displayFilms .film').cloneNode(true);
+
+            section.appendChild(article);
+
+            article.querySelector('h2').innerHTML = data[i].title;
+
+            let src = this.imgPath + "w500" + data[i].poster_path;
+            let image = article.querySelector('img');
+            image.setAttribute('src', src);
+            image.setAttribute('alt', data[i].title);
+
+            article.querySelector('a').setAttribute('href', 'fiche-film.html?id=' + data[i].id);
+
+            article.querySelector('.description').innerHTML = data[i].overview || "Aucune description disponible";
+
+            article.querySelector('.vote').innerHTML = "Moyenne des votes : " + data[i].vote_average;
+
+            article.querySelector('.annee').innerHTML = "Date de sortie : " + data[i].release_date;
+
+
+        }
+    }
+
+
+    requeteDernierFilmsCarrousel() {
+        let requette = new XMLHttpRequest();
+        requette.addEventListener('loadend', this.retourRequeteDernierFilmCarrousel.bind(this));
+        requette.open('GET', this.baseUrl + 'movie/popular?api_key=' + this.apikey + '&language=' + this.lang + '&page=1');
+        requette.send();
+    };
+
+    retourRequeteDernierFilmCarrousel(event) {
+        let target = event.currentTarget;
+        let data = JSON.parse(target.responseText).results;
+        //console.log(data);
+
+        this.afficheDernierFilmCarrousel(data);
+
+    };
+
+    afficheDernierFilmCarrousel(data) {
+        let section2 = document.querySelector('.swiper-wrapper');
+
+        for (let i = 0; i < this.totalFilm; i++) {
+            let article = document.querySelector('.swiper-wrapper .swiper-slide').cloneNode(true);
+
+            section2.appendChild(article);
+
+            article.querySelector('h2').innerHTML = data[i].title;
+
+            let src = this.imgPath + "w500" + data[i].poster_path;
+            let image = article.querySelector('img');
+            image.setAttribute('src', src);
+            image.setAttribute('alt', data[i].title);
+
+           article.querySelector('a').setAttribute('href', 'fiche-film.html?id=' + data[i].id);
+
+           article.querySelector('.description').innerHTML = data[i].overview || "Aucune description disponible";
+
+           article.querySelector('.vote').innerHTML = "Moyenne des votes : " + data[i].vote_average;
 
 
 
+        }
+    }
 
+
+
+    requeteInfoFilms(movieId) {
+        let requette = new XMLHttpRequest();
+        requette.addEventListener('loadend', this.retourRequeteInfoFilm.bind(this));
+        requette.open('GET', this.baseUrl + 'movie/' + movieId +'?api_key=' + this.apikey + '&language=' + this.lang);
+        requette.send();
+    };
+
+    retourRequeteInfoFilm(event) {
+        console.log('ca marche');
+        let target = event.currentTarget;
+        let data = JSON.parse(target.responseText);
+        console.log(data.title);
+        console.log(target.responseText);
+
+        this.afficheInfoFilm(data);
+    };
+
+    afficheInfoFilm(data) {
+
+        //requetteActeur()
+        document.querySelector('h1').innerHTML = data.title;
+
+
+    }
+
+    requetteActeur(movieId){
+        //GET CREDIT (movieDB) - requette AJAX
+
+    }
+
+    retourRequetteActeur(){
+        //Faire attention au JSON.parse... il ny a pas de results
+
+    }
+
+    afficheActeur(){
+        //boucle pour afficher tous les acteurs avec un cloneNode
+
+    }
+
+
+}
 
 
 
